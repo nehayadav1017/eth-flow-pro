879a9b3f2fc2b00245fa83a47919f13d5ac42061
nehayadav1017 <nehayadav1017@gmail.com>
2026-06-06 13:55:53 +0530

    Update collector.js

--- collector.js
+++ collector.js
@@ -1,13 +1,21 @@
-
 import fetch from "node-fetch";
 import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPABASE_SERVICE_ROLE = process.env.SUPABASE_SERVICE_ROLE;
+const SUPABASE_SERVICE_KEY =
+  process.env.SUPABASE_SERVICE_KEY;
+
+if (!SUPABASE_URL) {
+  throw new Error("SUPABASE_URL missing");
+}
+
+if (!SUPABASE_SERVICE_KEY) {
+  throw new Error("SUPABASE_SERVICE_KEY missing");
+}
 
 const supabase = createClient(
   SUPABASE_URL,
-  SUPABASE_SERVICE_ROLE
+  SUPABASE_SERVICE_KEY
 );
 
 const API_BASE = "https://api.india.delta.exchange/v2";
